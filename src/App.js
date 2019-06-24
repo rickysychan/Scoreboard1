683cc076560cd40d1cef2f382e02df6e2d4e79c7
ricky <rickysychan@gmail.com>
2019-06-24 17:56:24 -0400

    added some inital set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const initalData = {
+  players: [
+    {
+      name: "Jim Hoskins",
+      score: 31,
+      created: "11/8/2016",
+      updated: "11/9/2016"
+    },
+    {
+      name: "Andrew Chalkley",
+      score: 20,
+      created: "11/9/2016",
+      updated: "11/10/2016"
+    },
+    {
+      name: "Alena Holligan",
+      score: 50,
+      created: "11/11/2016",
+      updated: "11/12/2016"
+    }
+  ]
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="scoreboard">Scoreboard</div>;
 }
 
 export default App;
